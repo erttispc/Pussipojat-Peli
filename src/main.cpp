cd47c2f5dec6825e5c019ad5134667bdcb5a566d
Eero Mäntyvaara <eero.mantyvaara@gmail.com>
2025-10-29 11:55:22 +0200

    Pääkoodi viel

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,63 @@
-#include <Arduino.h>
+#include "display.h"
+#include "buttons.h"
+#include "leds.h"
+#include "SpedenSpelit.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Use these 2 volatile variables for communicating between
+// loop() function and interrupt handlers
+volatile int buttonNumber = -1;           // for buttons interrupt handler
+volatile bool newTimerInterrupt = false;  // for timer interrupt handler
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void setup()
+{
+  /*
+    Initialize here all modules
+  */
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if(buttonNumber>=0)
+  {
+     // start the game if buttonNumber == 4
+     // check the game if 0<=buttonNumber<4
+  }
+
+  if(newTimerInterrupt == true)
+  {
+     // new random number must be generated
+     // and corresponding let must be activated
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void initializeTimer(void)
+{
+	// see requirements for the function from SpedenSpelit.h
 }
+ISR(TIMER1_COMPA_vect)
+{
+  /*
+  Communicate to loop() that it's time to make new random number.
+  Increase timer interrupt rate after 10 interrupts.
+  */
+  
+}
+
+
+void checkGame(byte nbrOfButtonPush)
+{
+	// see requirements for the function from SpedenSpelit.h
+}
+
+
+void initializeGame()
+{
+	// see requirements for the function from SpedenSpelit.h
+}
+
+void startTheGame()
+{
+   // see requirements for the function from SpedenSpelit.h
+}
+
